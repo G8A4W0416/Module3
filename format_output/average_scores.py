a43d17aeb05bb2f400b442f28bd402c9b6fa8480
G8A4W0416 <wilhelmg1@gmail.com>
2020-02-04 21:34:29 -0600

    Completed average() method by getting three mock values from unit test and calculating the average score.

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,2 +1,8 @@
 def average():
-    pass
+    score_1_input = input("Please enter a score out of 100: ")
+    score_2_input = input("Please enter another score out of 100: ")
+    score_3_input = input("Please enter another score out of 100: ")
+    total_score = int(score_1_input) + int(score_2_input) + int(score_3_input)
+    average_score = total_score / 3
+
+    return average_score
